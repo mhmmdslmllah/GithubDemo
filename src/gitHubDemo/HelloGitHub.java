ccc585c2bba34adb32f98cca950236d73c461a1b
mhmmdslmllah <mhmmdslmllah@gmail.com>
2022-08-10 18:58:02 -0400

    homework

---
+++ src/gitHubDemo/HelloGitHub.java
@@ -0,0 +1,9 @@
+package gitHubDemo;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+System.out.println("I know how to create a seperate repositry in Github and can push my project GithubDemo, without help of anyone");
+	}
+
+}
